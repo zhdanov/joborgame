18306e51adcca7437309250787cab22f04b49598
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-28 21:34:01 +0300

    fix(post-start): gitlab prod environment

--- container-magic-post-start.bash
+++ container-magic-post-start.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if nc -z -w2 gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT 80 2>/dev/null
+if nc -z -w2 gitlab-prod.gitlab-prod 80 2>/dev/null
 then
 
-    printf "Host gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
+    printf "Host gitlab-prod.gitlab-prod\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
 
     if [[ ! -d /var/www/joborgame-backend ]]; then
-        git clone git@gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT:$HOME_USER_NAME/joborgame-backend.git /var/www/joborgame-backend
+        git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/joborgame-backend.git /var/www/joborgame-backend
         pushd /var/www/joborgame-backend/daemons/magic/
             npm i
         popd
--- container-php-fpm-post-start.bash
+++ container-php-fpm-post-start.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if nc -z -w2 gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT 80 2>/dev/null
+if nc -z -w2 gitlab-prod.gitlab-prod 80 2>/dev/null
 then
 
-    printf "Host gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
+    printf "Host gitlab-prod.gitlab-prod\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
 
     if [[ ! -d /var/www/joborgame-backend ]]; then
-        git clone git@gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT:$HOME_USER_NAME/joborgame-backend.git /var/www/joborgame-backend
+        git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/joborgame-backend.git /var/www/joborgame-backend
     fi
 
     [ -d /usr/local/composer-store ] && rm -rf /usr/local/composer-store && mkdir -p /usr/local/composer-store
 
-    git clone git@gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT:$HOME_USER_NAME/hangar.git /usr/local/composer-store/hangar
+    git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/hangar.git /usr/local/composer-store/hangar
     pushd /usr/local/composer-store/hangar
         git archive --format zip --output ../hangar.zip master
     popd
 
 
-    git clone git@gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT:$HOME_USER_NAME/samovar.git /usr/local/composer-store/samovar
+    git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/samovar.git /usr/local/composer-store/samovar
     pushd /usr/local/composer-store/samovar
         git archive --format zip --output ../samovar.zip master
     popd
